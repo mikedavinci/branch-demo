ba99232458eef84c4abfc034f00d6398b0ebe2fb
Miguel Esparza Jr <mike@mikedavinci.com>
2022-12-10 11:48:52 -0600

    build

---
+++ build/precache-manifest.a01f1a568343628483e38270a8100c75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9e0fc90abf80416c38ce4c076b3b60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1657f6b28c7786e7b76",
+    "url": "/static/css/main.ed051695.chunk.css"
+  },
+  {
+    "revision": "81a4ad6c6f74e3430d69",
+    "url": "/static/js/2.e6b5aa87.chunk.js"
+  },
+  {
+    "revision": "f1657f6b28c7786e7b76",
+    "url": "/static/js/main.31e8b6c3.chunk.js"
+  },
+  {
+    "revision": "6ee2bde56fa8bff09874",
+    "url": "/static/js/runtime~main.d7ec23f5.js"
+  }
+]);
